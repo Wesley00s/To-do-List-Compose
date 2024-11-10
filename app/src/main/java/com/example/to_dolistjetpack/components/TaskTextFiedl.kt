666fcadafd4bcd75401e39c77645d2ca30ce2368
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-10 01:31:35 -0300

    feat: create Task Text Field component

---
+++ app/src/main/java/com/example/to_dolistjetpack/components/TaskTextFiedl.kt
@@ -0,0 +1,43 @@
+package com.example.to_dolistjetpack.components
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.example.to_dolistjetpack.ui.theme.LightBlue
+
+@Composable
+fun TaskTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier,
+    label: String,
+    maxLines: Int,
+    keyboardType: KeyboardType
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        label = {
+            Text(text = label)
+        },
+        maxLines = maxLines,
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = LightBlue,
+            focusedLabelColor = LightBlue,
+            unfocusedLabelColor = Color.Gray,
+            cursorColor = LightBlue
+        ),
+        shape = RoundedCornerShape(10.dp),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType
+        )
+    )
+}
