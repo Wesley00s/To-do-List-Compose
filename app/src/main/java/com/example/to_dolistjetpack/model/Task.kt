d5f4fd5c4371f94ad6224b93ce6038cd0c89650a
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-13 01:26:37 -0300

    feat: add id on task model

--- app/src/main/java/com/example/to_dolistjetpack/model/Task.kt
+++ app/src/main/java/com/example/to_dolistjetpack/model/Task.kt
@@ -1,12 +1,12 @@
 package com.example.to_dolistjetpack.model
 
 import com.example.to_dolistjetpack.enumeration.PriorityLevel
-import java.time.LocalDateTime
 
 data class Task(
+    var id: String? = null,
     val name: String? = null,
     val description: String? = null,
     val priority: PriorityLevel? = null,
-    val updateAt: LocalDateTime? = null,
+    val updateAt: String = "",
     var isDone: Boolean? = null
 )
