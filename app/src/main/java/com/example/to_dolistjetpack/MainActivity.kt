85ba6596005f01440f2a3749f957ed0efbb27db4
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-13 01:32:34 -0300

    feat: add splash screeen animated

--- app/src/main/java/com/example/to_dolistjetpack/MainActivity.kt
+++ app/src/main/java/com/example/to_dolistjetpack/MainActivity.kt
@@ -14,11 +14,13 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.to_dolistjetpack.ui.theme.TodoListJetPackTheme
-import com.example.to_dolistjetpack.view.auth.LoginView
-import com.example.to_dolistjetpack.view.auth.RegisterView
+import com.example.to_dolistjetpack.view.EditTask
 import com.example.to_dolistjetpack.view.SaveTask
+import com.example.to_dolistjetpack.view.SplashScreen
 import com.example.to_dolistjetpack.view.ToDoList
+import com.example.to_dolistjetpack.view.auth.LoginView
 import com.example.to_dolistjetpack.view.auth.PasswordRecoveryView
+import com.example.to_dolistjetpack.view.auth.RegisterView
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
@@ -30,25 +32,37 @@ class MainActivity : ComponentActivity() {
             TodoListJetPackTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) {
                     val navController = rememberNavController()
-
                     NavHost(
                         navController = navController,
-                        startDestination = "login",
+                        startDestination = "splash",
                     ) {
+                        composable("splash") {
+                            SplashScreen {
+                                navController.navigate("login") {
+                                    popUpTo("splash") { inclusive = true }
+                                }
+                            }
+                        }
                         composable("home") {
-                            ToDoList(navController)
+                            ToDoList(navController, this@MainActivity)
                         }
                         composable("saveTask") {
-                            SaveTask(navController)
+                            SaveTask(navController, this@MainActivity)
                         }
                         composable("login") {
-                            LoginView(navController)
+                            LoginView(navController, this@MainActivity)
                         }
                         composable("register") {
-                            RegisterView(navController,this@MainActivity)
+                            RegisterView(navController, this@MainActivity)
                         }
                         composable("passwordRecovery") {
-                            PasswordRecoveryView(navController)
+                            PasswordRecoveryView(navController, this@MainActivity)
+                        }
+                        composable("editTask/{taskId}") { backStackEntry ->
+                            val taskId = backStackEntry.arguments?.getString("taskId")
+                            if (taskId != null) {
+                                EditTask(navController, this@MainActivity, taskId)
+                            }
                         }
                     }
                 }
