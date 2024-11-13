12d14a7020c34adfe741e4404fc351a3b93a9eef
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-13 01:30:26 -0300

    feat: implement create task on firebase

--- app/src/main/java/com/example/to_dolistjetpack/view/SaveTask.kt
+++ app/src/main/java/com/example/to_dolistjetpack/view/SaveTask.kt
@@ -1,5 +1,8 @@
 package com.example.to_dolistjetpack.view
 
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -27,24 +30,43 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.to_dolistjetpack.R
 import com.example.to_dolistjetpack.components.TaskButton
 import com.example.to_dolistjetpack.components.TaskTextField
+import com.example.to_dolistjetpack.enumeration.PriorityLevel
+import com.example.to_dolistjetpack.model.Task
+import com.example.to_dolistjetpack.ui.theme.LightBlue
 import com.example.to_dolistjetpack.ui.theme.MediumGreen
 import com.example.to_dolistjetpack.ui.theme.MediumRed
 import com.example.to_dolistjetpack.ui.theme.MediumYellow
 import com.example.to_dolistjetpack.ui.theme.Tertiary
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
+import java.time.LocalDateTime
 
+@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SaveTask(
-    navController: NavController
+    navController: NavController,
+    context: Context
 ) {
+    var taskName by remember { mutableStateOf("") }
+    var taskDescription by remember { mutableStateOf("") }
+    var selectedPriority by remember { mutableStateOf<String?>(null) }
+    val datasource = Firebase.database.reference.child("users")
+    val userId = Firebase.auth.currentUser?.uid!!
+
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -53,14 +75,14 @@ fun SaveTask(
                 ),
                 title = {
 
-                        Text(
-                            text = "Create Task",
-                            style = TextStyle(
-                                fontSize = 18.sp,
-                                fontWeight = FontWeight.Bold,
-                                color = Color.LightGray
-                            )
+                    Text(
+                        text = "Create Task",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.LightGray
                         )
+                    )
 
                 },
                 navigationIcon = {
@@ -78,23 +100,19 @@ fun SaveTask(
         },
     ) { paddingValues ->
 
-        var taskName by remember { mutableStateOf("") }
-        var taskDescription by remember { mutableStateOf("") }
-        var noPriority by remember { mutableStateOf(false) }
-        var selectedPriority by remember { mutableStateOf<String?>(null) }
-
         Column(
             modifier = Modifier
                 .padding(paddingValues)
                 .verticalScroll(rememberScrollState()),
         ) {
-            Column (
+            Column(
                 modifier = Modifier
                     .padding(20.dp)
                     .fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.spacedBy(16.dp)
-            ){
+            ) {
+
                 TaskTextField(
                     value = taskName,
                     onValueChange = {
@@ -104,7 +122,11 @@ fun SaveTask(
                     label = "Task Name",
                     maxLines = 1,
                     keyboardType = KeyboardType.Text,
-                    singleLine = true
+                    singleLine = true,
+                    focusedBorderColor = LightBlue,
+                    leadingIcon = ImageVector.vectorResource(id = R.drawable.ic_name),
+                    isValid = { taskName.isNotBlank() },
+                    errorMessage = "Task name cannot be empty"
                 )
                 TaskTextField(
                     value = taskDescription,
@@ -117,7 +139,9 @@ fun SaveTask(
                     label = "Task Description",
                     maxLines = 5,
                     keyboardType = KeyboardType.Text,
-                    singleLine = false
+                    singleLine = false,
+                    focusedBorderColor = LightBlue,
+                    leadingIcon = ImageVector.vectorResource(id = R.drawable.ic_description)
                 )
 
                 Row(
@@ -162,12 +186,32 @@ fun SaveTask(
                 }
                 TaskButton(
                     onClick = {
-
+                        val newTask = Task(
+                            name = taskName,
+                            description = taskDescription,
+                            priority = when (selectedPriority) {
+                                "low" -> PriorityLevel.LOW
+                                "medium" -> PriorityLevel.MEDIUM
+                                "high" -> PriorityLevel.HIGH
+                                else -> PriorityLevel.NONE
+                            },
+                            updateAt = LocalDateTime.now().toString(),
+                            isDone = false
+                        )
+                        createTask(datasource, userId, newTask)
+                        navController.popBackStack()
                     },
                     enabled = taskName.isNotBlank(),
                     text = "Save",
-                    modifier = Modifier.fillMaxWidth())
+                    modifier = Modifier.fillMaxWidth(),
+                )
             }
         }
     }
+}
+
+fun createTask(datasource: DatabaseReference, userId: String, task: Task) {
+    val userTasksRef = datasource.child(userId).child("tasks").push()
+    task.id = userTasksRef.key
+    userTasksRef.setValue(task)
 }
