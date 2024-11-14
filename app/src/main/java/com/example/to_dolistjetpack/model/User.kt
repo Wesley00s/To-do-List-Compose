3d1b34a08e800b967f88f1f552046099c4445903
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-14 00:51:38 -0300

    feat: create user model class

---
+++ app/src/main/java/com/example/to_dolistjetpack/model/User.kt
@@ -0,0 +1,9 @@
+package com.example.to_dolistjetpack.model
+
+data class User(
+    var id: String? = null,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val email: String? = null,
+    val password: String? = null,
+)
