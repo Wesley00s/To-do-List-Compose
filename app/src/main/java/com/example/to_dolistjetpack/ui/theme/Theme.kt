581606f6be9a359bb6592f3f401f6f1b19471d69
Wesley00s <wesley300rodrigues@gmail.com>
2024-11-14 00:49:27 -0300

    refactor: add is system in theme dark method

--- app/src/main/java/com/example/to_dolistjetpack/ui/theme/Theme.kt
+++ app/src/main/java/com/example/to_dolistjetpack/ui/theme/Theme.kt
@@ -1,6 +1,7 @@
 package com.example.to_dolistjetpack.ui.theme
 
 import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -33,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun TodoListJetPackTheme(
-    darkTheme: Boolean = false,
+    darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
