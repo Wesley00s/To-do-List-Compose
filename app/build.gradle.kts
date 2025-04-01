469b2c5900948fff6890db0043750ccfad5d9131
Wesley00s <wesley300rodrigues@gmail.com>
2025-04-01 09:58:56 -0300

    chore: add firestore dependency in build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.database.ktx)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
