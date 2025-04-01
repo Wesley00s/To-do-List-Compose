12a847b9d4479ff858ed2d67ed1e3add62d29f0a
Wesley00s <wesley300rodrigues@gmail.com>
2025-04-01 10:11:25 -0300

    chore: remove log

--- app/src/main/java/com/example/to_dolistjetpack/repository/TaskRepository.kt
+++ app/src/main/java/com/example/to_dolistjetpack/repository/TaskRepository.kt
@@ -19,8 +19,6 @@ class TaskRepository(private val db: FirebaseFirestore) {
         taskRef.set(task)
             .addOnFailureListener {
                 Toast.makeText(context, "Failed to create task", Toast.LENGTH_SHORT).show()
-                Log.d("TaskRepository", "createTask: ${it.message}")
-
             }
     }
 
