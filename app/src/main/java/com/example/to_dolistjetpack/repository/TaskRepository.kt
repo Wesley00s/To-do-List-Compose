5993c9f1bfaeb2aad516aa8d7f492afa9964b451
Wesley00s <wesley300rodrigues@gmail.com>
2025-04-01 10:08:40 -0300

    refactor: refactor task repository to using firestore database

--- app/src/main/java/com/example/to_dolistjetpack/repository/TaskRepository.kt
+++ app/src/main/java/com/example/to_dolistjetpack/repository/TaskRepository.kt
@@ -1,15 +1,27 @@
 package com.example.to_dolistjetpack.repository
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 import com.example.to_dolistjetpack.model.Task
-import com.google.firebase.database.DatabaseReference
+import com.google.firebase.firestore.FirebaseFirestore
 
-class TaskRepository(private val datasource: DatabaseReference) {
-    fun createTask(userId: String, task: Task) {
-        val userTasksRef = datasource.child(userId).child("tasks").push()
-        task.id = userTasksRef.key
-        userTasksRef.setValue(task)
+class TaskRepository(private val db: FirebaseFirestore) {
+
+    fun createTask(
+        context: Context,
+        userId: String, task: Task
+    ) {
+        val taskRef = db.collection("users").document(userId)
+            .collection("tasks").document()
+
+        task.id = taskRef.id
+        taskRef.set(task)
+            .addOnFailureListener {
+                Toast.makeText(context, "Failed to create task", Toast.LENGTH_SHORT).show()
+                Log.d("TaskRepository", "createTask: ${it.message}")
+
+            }
     }
 
     fun deleteTask(
@@ -18,16 +30,17 @@ class TaskRepository(private val datasource: DatabaseReference) {
         taskId: String,
         onDeleted: (Boolean) -> Unit
     ) {
-        val taskRef = datasource.child(userId).child("tasks").child(taskId)
-        taskRef.removeValue().addOnCompleteListener { task ->
-            if (task.isSuccessful) {
+        db.collection("users").document(userId)
+            .collection("tasks").document(taskId)
+            .delete()
+            .addOnSuccessListener {
                 Toast.makeText(context, "Task deleted", Toast.LENGTH_SHORT).show()
                 onDeleted(true)
-            } else {
+            }
+            .addOnFailureListener {
                 Toast.makeText(context, "Failed to delete task", Toast.LENGTH_SHORT).show()
                 onDeleted(false)
             }
-        }
     }
 
     fun editTask(
@@ -36,37 +49,28 @@ class TaskRepository(private val datasource: DatabaseReference) {
         task: Task,
         onEdited: (Boolean) -> Unit
     ) {
-        val taskRef = datasource.child(userId).child("tasks").child(taskId)
-
         val updates = mapOf(
             "name" to task.name,
             "description" to task.description,
             "priority" to task.priority,
-            "isDone" to task.isDone,
+            "done" to task.isDone,
             "updateAt" to task.updateAt
         )
 
-        taskRef.updateChildren(updates).addOnCompleteListener {
-            if (it.isSuccessful) {
-                onEdited(true)
-            } else {
-                onEdited(false)
-            }
-        }
+        db.collection("users").document(userId)
+            .collection("tasks").document(taskId)
+            .update(updates)
+            .addOnSuccessListener { onEdited(true) }
+            .addOnFailureListener { onEdited(false) }
     }
 
     fun completeTask(
         userId: String,
         taskId: String,
-        task: Task
+        isDone: Boolean
     ) {
-        val taskRef = datasource.child(userId).child("tasks").child(taskId)
-
-        val updates = mapOf(
-            "done" to task.isDone,
-        )
-
-        taskRef.updateChildren(updates)
-
+        db.collection("users").document(userId)
+            .collection("tasks").document(taskId)
+            .update("done", isDone)
     }
 }
