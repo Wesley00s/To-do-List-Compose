0276648a09c8b1d27a410d943a40ed8382d7bf33
Wesley00s <wesley300rodrigues@gmail.com>
2025-04-04 11:17:42 -0300

    fix: fix card height

--- app/src/main/java/com/example/to_dolistjetpack/components/TaskItem.kt
+++ app/src/main/java/com/example/to_dolistjetpack/components/TaskItem.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -100,7 +99,6 @@ fun TaskItem(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .height(180.dp)
             .scale(cardScale)
             .padding(8.dp)
             .clickable {
@@ -119,11 +117,11 @@ fun TaskItem(
                 .padding(top = 5.dp, bottom = 5.dp, end = 10.dp)
                 .fillMaxWidth(),
         ) {
-            Row (
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(start = 10.dp)
-            ){
+            ) {
                 Checkbox(
                     checked = isChecked,
                     onCheckedChange = {
@@ -133,8 +131,9 @@ fun TaskItem(
                         vibrateOnClick(context)
                     },
                     modifier = Modifier
-                        .scale(1.2f).align(Alignment.CenterVertically)
-                    ,colors = CheckboxDefaults.colors(
+                        .scale(1.2f)
+                        .align(Alignment.CenterVertically),
+                    colors = CheckboxDefaults.colors(
                         checkedColor = LightBlue,
                         uncheckedColor = Color.Gray,
                         checkmarkColor = Color.White
@@ -142,7 +141,7 @@ fun TaskItem(
                 )
                 Column {
                     Text(
-                        text = task.name.toString(),
+                        text = task.name,
                         modifier = Modifier.padding(top = 10.dp, start = 20.dp, end = 20.dp),
                         style = TextStyle(
                             fontSize = 18.sp,
@@ -170,7 +169,7 @@ fun TaskItem(
                     }
 
                     Text(
-                        text = task.description.toString(),
+                        text = task.description,
                         style = TextStyle(
                             fontSize = 15.sp,
                             fontWeight = FontWeight.Bold,
@@ -178,7 +177,9 @@ fun TaskItem(
                             textDecoration = if (isChecked) TextDecoration.LineThrough else TextDecoration.None,
                             fontStyle = if (isChecked) FontStyle.Italic else FontStyle.Normal
                         ),
-                        modifier = Modifier.padding(start = 20.dp, end = 10.dp, top = 5.dp)
+                        modifier = Modifier.padding(start = 20.dp, end = 10.dp, top = 5.dp),
+                        maxLines = 5,
+                        overflow = TextOverflow.Ellipsis
                     )
 
                     Row(
